5f8919ae2935e43eac61d050569811f2adbb2287
Purushottam Balaka <122111835+purushottam-balaka@users.noreply.github.com>
2024-01-03 17:10:52 +0530

    Update app.js

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ app.use(route);
 //     }
 // }))
 app.use(compression());
-app.use(morgan('combined',{stream:accessLogStream}));
+//app.use(morgan('combined',{stream:accessLogStream}));
 app.use(express.static('views'));
 app.use(cors());
 
