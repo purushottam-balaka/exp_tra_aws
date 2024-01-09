ac7677a6926be9b0d29e441c02f842fd963a8528
Purushottam Balaka <122111835+purushottam-balaka@users.noreply.github.com>
2024-01-09 16:37:07 +0530

    Update login.js

--- views/login.js
+++ views/login.js
@@ -6,7 +6,7 @@ async function login_save(e){
     gmail:e.target.gmail.value,
     password:e.target.pwd.value,
         }
-    const resp= await axios.post(`http://3.110.32.173:9000/login`,loginDetails);
+    const resp= await axios.post(`http://65.2.167.147:9000/login`,loginDetails);
     localStorage.setItem('token',resp.data.token);
     window.location.href="./expenseTracker.html";
 
