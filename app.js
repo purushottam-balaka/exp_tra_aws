7ff64d43f7c42500a7fd75180c0db548b750c390
Purushottam Balaka <122111835+purushottam-balaka@users.noreply.github.com>
2024-01-10 17:32:05 +0530

    Update app.js

--- app.js
+++ app.js
@@ -26,13 +26,8 @@ app.use(compression());
 //app.use(morgan('combined',{stream:accessLogStream}));
 app.use(express.static('views'));
 
-// app.use(cors());
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-  next();
-})
+app.use(cors());
+
 users.hasMany(expenses);
 expenses.belongsTo(users);
 
